e0b8914716b7b2ed7a61660cb76600a6ce85976b
Robert Kulp <pedalinon@gmail.com>
2020-11-05 15:20:37 -0800

    Almost done with word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,16 +1,28 @@
-str = "Hello, my cat. And my cat doesn't say 'hello' back!"
 
-res = len(str.split())
-print(str, 'length', res)
+# if 'cats' in res:
+# 	print(True)
+# else:
+# 	print(False)
 
 def word_count(s):
     # Your code here
-    pass
+    cache = {}
+    str = s.split()
+    a = 0
+
+    for i in str:
+    	if i in str:
+    		a = a + 1
+    		print(a)
+    	# print(i)
+    return cache
 
 
 
-if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
+# if __name__ == "__main__":
+print(word_count(""))
+print(word_count("Hello"))
+print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
